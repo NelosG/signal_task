0f0d1eaecdf0af7b8ea59c9b78e22a86c71fb660
NelosG <gleb.pushkarev@mail.ru>
2020-10-28 07:48:49 +0300

    Что-то подобное?

--- signals.h
+++ signals.h
@@ -5,140 +5,116 @@
 #include "intrusive_list.h"
 
 namespace signals {
-    template<typename T>
-    class signal;
+template <typename T> class signal;
 
-    template<typename... Args>
-    class signal<void(Args...)> {
-    private:
+template <typename... Args> class signal<void(Args...)> {
+private:
+  using slot_t = std::function<void(Args...)>;
+  struct connection_tag;
 
-        using slot_t = std::function<void(Args...)>;
-        struct follower_tag;
+public:
+  class connection : public intrusive::list_element<connection_tag> {
+  public:
+    connection() = default;
 
-        class base_follower : public intrusive::list_element<follower_tag> {
-            using base_t = intrusive::list_element<follower_tag>;
+    connection(signal *sig, slot_t slot) noexcept
+        : sig(sig), slot(std::move(slot)) {
+      sig->followers.push_front(*this);
+    }
 
-        public:
+    connection(connection &&other) noexcept
+        : slot(std::move(other.slot)), sig(other.sig) {
+      sig->followers.insert(followers_list::iterator(&other), *this);
+      other.unlink();
+      change_enclosing_iterators(
+          other, [&](auto) { return followers_list::iterator(this); });
+    }
+    void disconnect() noexcept {
+      if(isLinked()) {
 
-            base_follower() = default;
+        change_enclosing_iterators(*this,
+                                   [](auto cur) { return std::next(cur); });
+        unlink();
+        slot = {};
+        sig = nullptr;
+      }
+    }
 
-            explicit base_follower(signal *sig) noexcept: signal_e(sig) {
-                sig->followers.push_front(*this);
-            }
+    connection &operator=(connection &&other) noexcept {
+      if (this != &other) {
+        disconnect();
+        sig = other.sig;
+        slot = std::move(other.slot);
+        sig->followers.insert(followers_list::iterator(&other), *this);
+        other.unlink();
+        change_enclosing_iterators(
+            other, [&](auto) { return followers_list::iterator(this); });
+      }
+      return *this;
+    }
 
-            base_follower(base_follower &&other) noexcept: base_t(std::move(other)), signal_e(other.signal_e) {
-                change_enclosing_iterators(other,
-                                           [&](auto) { return followers_list::iterator(this); });
-            }
+    ~connection() { disconnect(); }
 
-            base_follower &operator=(base_follower &&other) noexcept {
-                this->prev = other.prev;
-                this->next = &other;
-                if (other.prev != nullptr)
-                    other.prev->next = this;
-                other.prev = this;
-                other.unlink();
+  private:
+    template <class IteratorSupplier>
+    void change_enclosing_iterators(const connection &other,
+                                    IteratorSupplier supplier) const noexcept {
+      if (isLinked())
+        for (iteration_stack *el = sig->top_iterator; el; el = el->next)
+          if (el->current != sig->followers.end() && &*el->current == &other) {
+            el->current = supplier(el->current);
+          }
+    }
+    friend class signal;
+    signal *sig;
+    slot_t slot;
+  };
 
-                signal_e = other.signal_e;
-                change_enclosing_iterators(other,
-                                           [&](auto) { return followers_list::iterator(this); });
-                return *this;
-            }
+  signal() = default;
 
-            void disconnect() noexcept {
-                change_enclosing_iterators(*this, [](auto cur) { return std::next(cur); });
-                unlink();
-            }
+  signal(const signal &) = delete;
 
-            ~base_follower() {
-                change_enclosing_iterators(*this, [](auto cur) { return std::next(cur); });
-            }
+  signal &operator=(const signal &) = delete;
 
-        private:
-            template<class IteratorSupplier>
-            void change_enclosing_iterators(const base_follower &other,
-                                            IteratorSupplier supplier) const noexcept {
-                if (isLinked())
-                    for (iteration_stack *el = signal_e->top_iterator; el; el = el->next)
-                        if (el->current != signal_e->followers.end() && &*el->current == &other) {
-                            el->current = supplier(el->current);
-                            return;
-                        }
-            }
+  ~signal() {
+    for (iteration_stack *el = top_iterator; el != nullptr; el = el->next) {
+      el->sig = nullptr;
+    }
+  }
 
-            signal *signal_e;
-        };
+  connection connect(slot_t slot) noexcept {
+    return connection(this, std::move(slot));
+  }
 
-    public:
+  void operator()(Args... args) const {
+    iteration_stack el(this);
+    while (el.current != followers.end()) {
+      auto copy = el.current++;
+      copy->slot(args...);
+      if (el.sig == nullptr)
+        return;
+    }
+  }
 
-        class connection : public base_follower {
-        public:
+private:
+  using followers_list = intrusive::list<connection, connection_tag>;
 
-            connection() = default;
+  struct iteration_stack {
+    explicit iteration_stack(signal const *sig)
+        : sig(sig), current(sig->followers.begin()), next(sig->top_iterator) {
+      sig->top_iterator = this;
+    }
+    ~iteration_stack() {
+      if (sig != nullptr) {
+        sig->top_iterator = next;
+      }
+    }
+    typename followers_list::const_iterator current;
+    iteration_stack *next;
+    signal const *sig;
+  };
 
-            connection(signal *sig, slot_t slot) noexcept
-                    : base_follower(sig), slot(std::move(slot)) {}
-
-            connection(connection &&) noexcept = default;
-
-            connection &operator=(connection &&) noexcept = default;
-
-            ~connection() = default;
-
-        private:
-
-            friend class signal;
-            slot_t slot;
-        };
-
-        signal() = default;
-
-        signal(const signal &) = delete;
-
-        signal &operator=(const signal &) = delete;
-
-        ~signal() {
-            if (top_iterator != nullptr)
-                top_iterator->destroyed = true;
-        }
-
-        connection connect(slot_t slot) noexcept {
-            return connection(this, slot);
-        }
-
-        void operator()(Args... args) const {
-            iteration_stack el = {followers.begin(), top_iterator, &top_iterator};
-            top_iterator = &el;
-
-            while (el.current != followers.end()) {
-                auto &copy = static_cast<const connection &>(*(el.current++));
-                copy.slot(args...);
-                if (el.destroyed)
-                    return;
-            }
-        }
-
-    private:
-
-        using followers_list = intrusive::list<base_follower, follower_tag>;
-
-        struct iteration_stack {
-            typename followers_list::const_iterator current;
-            iteration_stack *next;
-            iteration_stack **next_ptr;
-            bool destroyed = false;
-
-            ~iteration_stack() {
-                if (destroyed) {
-                    if (next != nullptr)
-                        next->destroyed = true;
-                } else {
-                    *next_ptr = next;
-                }
-            }
-        };
-
-        followers_list followers;
-        mutable iteration_stack *top_iterator = nullptr;
-    };
+  followers_list followers;
+  mutable iteration_stack *top_iterator = nullptr;
+};
 }
